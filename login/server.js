eee2f6482ccd0ea8af3c21954d1401dd1d42707c
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-06-22 20:17:55 +0300

    add important files

---
+++ login/server.js
@@ -0,0 +1,28 @@
+const exporess = require('express');
+const app = exporess();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+
+app.get('/users/register', (req, res) =>{
+    res.render('register');
+});
+
+app.get('/users/login', (req, res) =>{
+    res.render('login');
+});
+
+app.get('/users/dashboard', (req, res) =>{
+    res.render('dashboard');
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
