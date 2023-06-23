5a54924b9bb640a0c4721be64f33098802c502ba
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-06-23 16:31:16 +0300

    removed unused imports

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 const express = require('express');
 const app = express();
-const { pool } = require('./config/dbConf');
 const session = require('express-session');
 const flash = require('express-flash');
 const passport = require('passport');
 const initializePassport = require('./config/passportConfig');
-const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3'); 
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const authRoutes = require("./routes/authRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes"); 
-
-const bucketName = process.env.BUCKET_NAME;
-const bucketRegion = process.env.BUCKET_REGION;
-const accessKey = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-
-const s3 = new S3Client({
-    credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secretAccessKey
-    },
-    region: bucketRegion
-});
 
 initializePassport(passport);
 
